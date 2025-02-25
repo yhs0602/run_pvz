0cccac18e892bb4916a0859a7812b990e52b0604
Hyeonseo Yang <yhs0602@snu.ac.kr>
2025-02-25 15:41:28 +0900

    🐛 Fix memory error

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <span>
 #include <vector>
 
 void disassemble_x86(const std::vector<uint8_t> &code, uint64_t address) {
@@ -29,42 +30,86 @@ void disassemble_x86(const std::vector<uint8_t> &code, uint64_t address) {
   cs_close(&handle);
 }
 
-void execute_one_instruction(const cs_insn &insn) {
-  std::cout << std::hex << insn.address << ": " << insn.mnemonic << " "
-            << insn.op_str << std::endl;
-  switch(insn.id) {
-    case X86_INS_CALL:
-      std::cout << "Call instruction detected! Detail:" << insn.detail << std::endl;
+// returns the offset of the next instruction
+int execute_one_instruction(const csh handle, const cs_insn *insn) {
+  std::cout << std::hex << insn->address << ": " << insn->mnemonic << " "
+            << insn->op_str << std::endl;
+  switch (insn->id) {
+  case X86_INS_CALL: {
+    std::cout << "Call instruction detected! Detail:" << insn->detail
+              << std::endl;
+    const cs_x86 &x86 = insn->detail->x86;
+
+    if (x86.op_count <= 0) {
+      std::cerr << "Error: No operands found!" << std::endl;
+      throw std::runtime_error("No operands found in CALL instruction!");
+    }
+    const cs_x86_op &op = x86.operands[0];
+    switch (op.type) {
+    case X86_OP_IMM:
+      std::cout << "Calling relative address: 0x" << std::hex << op.imm
+                << std::dec << std::endl;
+      return (insn->address + insn->size) + op.imm;
+    case X86_OP_MEM:
+      std::cout << "Indirect call via memory address" << std::endl;
+      std::cout << "Base: " << cs_reg_name(handle, op.mem.base) << std::endl;
+      std::cout << "Index: " << cs_reg_name(handle, op.mem.index) << std::endl;
+      std::cout << "Scale: " << op.mem.scale << std::endl;
+      std::cout << "Displacement: " << op.mem.disp << std::endl;
       break;
-    case X86_INS_RET:
-      std::cout << "Return instruction detected!" << std::endl;
+    case X86_OP_REG:
+      std::cout << "Indirect call via register: " << cs_reg_name(handle, op.reg)
+                << std::endl;
       break;
+    case X86_OP_INVALID:
+      std::cerr << "Invalid operand!" << std::endl;
+      throw std::runtime_error("Invalid operand in CALL instruction!");
+    }
+    break;
   }
+  case X86_INS_RET:
+    std::cout << "Return instruction detected!" << std::endl;
+    break;
+  }
+  return 0;
 }
 
-void execute_x86(const std::vector<uint8_t> &code, uint64_t address) {
+void execute_x86(const std::span<const uint8_t> &code, uint64_t start_offset,
+                 uint64_t address) {
   csh handle;
-  cs_insn insn;
   size_t count;
 
   if (cs_open(CS_ARCH_X86, CS_MODE_32, &handle) != CS_ERR_OK) {
     std::cerr << "Failed to initialize Capstone!" << std::endl;
     return;
   }
+  cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON);
+  cs_insn *insn = cs_malloc(handle);
+  if (insn == nullptr) {
+    std::cerr << "Failed to allocate memory for instruction!" << std::endl;
+    return;
+  }
+
   std::cout << "Executing from entry point...\n";
-  const uint8_t *code_ptr = code.data();
+
+  const uint8_t *current_code_ptr = (code.data() + start_offset);
   size_t code_size = code.size();
   uint64_t virtaddr = address;
   while (true) {
-    count = cs_disasm_iter(handle, &code_ptr, &code_size, &virtaddr, &insn);
+    count = cs_disasm_iter(handle, &current_code_ptr, &code_size, &virtaddr, insn);
     if (count == 1) {
-      execute_one_instruction(insn);
+      int next_instruction_offset = execute_one_instruction(handle, insn);
+      if (next_instruction_offset > 0) {
+        std::cout << "Next instruction offset: " << next_instruction_offset
+                  << std::endl;
+      }
       break;
     } else {
       std::cerr << "Disassembly failed!" << std::endl;
       break;
     }
   }
+  cs_free(insn, 1);
   cs_close(&handle);
 }
 
@@ -102,7 +147,6 @@ int main(int argc, char **argv) {
       text_section = const_cast<LIEF::PE::Section *>(&section);
     }
   }
-
   uint64_t entry_point_va = binary->optional_header().addressof_entrypoint();
   std::cout << "Entry Point Address (VA): 0x" << std::hex << entry_point_va
             << std::dec << std::endl;
@@ -127,22 +171,28 @@ int main(int argc, char **argv) {
   std::cout << "Calculated Entry Point Offset: 0x" << std::hex << entry_offset
             << std::dec << std::endl;
 
-  // PE 파일을 읽어서 엔트리포인트 근처의 바이트 가져오기
+  // Read the PE file to get the code section content
   std::ifstream pe_file(argv[1], std::ios::binary);
   if (!pe_file) {
     std::cerr << "Error opening PE file!" << std::endl;
     return 1;
   }
+  // Read the entire code section
+  auto text_content = text_section->content();
+  uint64_t entry_point_offset_in_text =
+      entry_point_va - text_section->virtual_address();
+  std::cout << "Entry Point Offset in .text section: 0x" << std::hex
+            << entry_point_offset_in_text << std::dec << std::endl;
 
-  // 엔트리포인트 주변 코드(256바이트) 읽기
-  pe_file.seekg(entry_offset);
-  std::vector<uint8_t> code(256);
-  pe_file.read(reinterpret_cast<char *>(code.data()), code.size());
+  // // 엔트리포인트 주변 코드(256바이트) 읽기
+  // pe_file.seekg(entry_offset);
+  // std::vector<uint8_t> code(256);
+  // pe_file.read(reinterpret_cast<char *>(code.data()), code.size());
 
-  // Capstone을 사용해 엔트리포인트부터 디스어셈블
-  std::cout << "\nDisassembling from entry point...\n";
-  disassemble_x86(code, entry_point_va);
-  execute_x86(code, entry_point_va);
+  // // Capstone을 사용해 엔트리포인트부터 디스어셈블
+  // std::cout << "\nDisassembling from entry point...\n";
+  // disassemble_x86(code, entry_point_va);
+  execute_x86(text_content, entry_point_offset_in_text, entry_point_va);
 
   return 0;
 }
